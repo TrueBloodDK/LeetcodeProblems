2a4a0b0c49d63f48b79a6a6b5308e2ddb5793a9d
denis <dkaras2016@gmail.com>
2025-10-26 19:49:53 +0500

    Solved problem 27

--- src/LC_0027.java
+++ src/LC_0027.java
@@ -46,13 +46,34 @@
 //    0 <= nums[i] <= 50
 //    0 <= val <= 100
 
+//
+ // SOLVED at 26.10.25
+//
+
+
+import java.util.Arrays;
 
 public class LC_0027 {
     public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(Arrays.toString(nums));
+        System.out.println(removeElement(nums, 2));
+        System.out.println(Arrays.toString(nums));
 
+        nums = new int[]{3, 2, 2, 3};
+        System.out.println(Arrays.toString(nums));
+        System.out.println(removeElement(nums, 3));
+        System.out.println(Arrays.toString(nums));
     }
 
-    public int removeElement(int[] nums, int val) {
-        return 1;
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
